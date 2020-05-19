bb975407a29aa4760b0d716d483d98fd5fbbadad
eunwoo1104 <sions04@naver.com>
2020-05-19 15:23:50 +0900

    fixed pager bug

--- Modules/pager.py
+++ Modules/pager.py
@@ -3,6 +3,9 @@ import asyncio
 
 async def auto_div_embed(bot, ctx, embed, result):
     emoji_list = ["⏹", "➡"]
+    if len(result) == 0:
+        embed.add_field(name="검색 결과 없음", value="이런! 검색 결과가 없네요...")
+        return
 
     def check(reaction, user):
         return str(reaction.emoji) in emoji_list and user == ctx.author
@@ -29,14 +32,15 @@ async def auto_div_embed(bot, ctx, embed, result):
         while True:
             reaction, user = await bot.wait_for("reaction_add", timeout=30, check=check)
             if str(reaction.emoji) == emoji_list[0]:
-                await msg.clear_reactions()
                 break
             elif str(reaction.emoji) == emoji_list[1]:
                 if len(result) == 0:
                     await ctx.send("더이상 결과가 없습니다.")
-                    await msg.clear_reactions()
                     break
-                await msg.remove_reaction(emoji_list[1], ctx.author)
+                try:
+                    await msg.remove_reaction(emoji_list[1], ctx.author)
+                except:
+                    pass
                 embed = embed
                 lim_count = 0
                 for x in result:
@@ -49,9 +53,18 @@ async def auto_div_embed(bot, ctx, embed, result):
                         break
                 await msg.edit(embed=embed)
                 embed.clear_fields()
+        try:
+            await msg.clear_reactions()
+        except:
+            await msg.remove_reaction(emoji_list[0], msg.author)
+            await msg.remove_reaction(emoji_list[1], msg.author)
 
     except asyncio.TimeoutError:
         ended_msg = await ctx.send("시간을 초과했습니다.")
-        await msg.clear_reactions()
+        try:
+            await msg.clear_reactions()
+        except:
+            await msg.remove_reaction(emoji_list[0], msg.author)
+            await msg.remove_reaction(emoji_list[1], msg.author)
         await ended_msg.delete(delay=5)
         return
