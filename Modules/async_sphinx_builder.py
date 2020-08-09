b4c7635ef849efbbf89664a254dd422144e66c95
eunwoo1104 <sions04@naver.com>
2020-08-09 11:11:57 +0900

    Fixed .nojekyll deleted

--- Modules/async_sphinx_builder.py
+++ Modules/async_sphinx_builder.py
@@ -13,7 +13,7 @@ async def build_dpdocs():
 
     shutil.copytree("discord.py-master/docs/_build/html", "temp")
     for file_list in os.listdir("docsweb"):
-        if file_list not in [".git", "CNAME", "README.md", "_static", "style.css"]:
+        if file_list not in [".git", "CNAME", "README.md", "_static", "style.css", ".nojekyll"]:
             try:
                 os.remove(f"docsweb/{file_list}")
                 shutil.copy(f"temp/{file_list}", f"docsweb/{file_list}")
